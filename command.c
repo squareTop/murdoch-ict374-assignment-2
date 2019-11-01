74e64ce09ac1abb6a5411b7eff22586549d8db93
Darius Baker <funkage@gmail.com>
2019-11-01 21:33:48 +0800

    refactor(command): add header file
    Closes #5

--- command.c
+++ command.c
@@ -1,24 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define SEPARATOR_CONCURRENT "&"
-#define SEPARATOR_INPUT      "<"
-#define SEPARATOR_OUTPUT     ">"
-#define SEPARATOR_PIPE       "|"
-#define SEPARATOR_SEQUENTIAL ";"
-#define MAX_COMMANDS  100
-#define MAX_ARGUMENTS 1000
-
-typedef struct CommandStruct {
-  char * name;
-  char * stdin;
-  char * stdout;
-  char * argv[MAX_ARGUMENTS];
-  int    argc;
-  int    background;
-  int    pipe;
-} Command;
+#include "command.h"
 
 /**
  * Helper command that prints Command attributes.
@@ -64,6 +44,28 @@ int get_arguments(char * input, Command * command) {
   }
 
   return argument_count;
+}
+
+/**
+ * Searches for a separator (;&|) and returns the character if found.
+ * @param  {char *} input
+ * @return {char *}
+ */
+char * get_separator(char * input) {
+  char * separator = NULL;
+
+  if ((separator = index(input, * SEPARATOR_SEQUENTIAL)) != NULL) {
+    // we found a ';' character
+    separator = SEPARATOR_SEQUENTIAL;
+  } else if ((separator = index(input, * SEPARATOR_CONCURRENT)) != NULL) {
+    // we found a '&' character
+    return SEPARATOR_CONCURRENT;
+  } else if ((separator = index(input, * SEPARATOR_PIPE)) != NULL) {
+    // we found a '|' character
+    return SEPARATOR_PIPE;
+  }
+
+  return separator;
 }
 
 /**
@@ -117,28 +119,6 @@ void make_command(
 }
 
 /**
- * Searches for a separator (;&|) and returns the character if found.
- * @param  {char *} input
- * @return {char *}
- */
-char * get_separator(char * input) {
-  char * separator = NULL;
-
-  if ((separator = index(input, * SEPARATOR_SEQUENTIAL)) != NULL) {
-    // we found a ';' character
-    separator = SEPARATOR_SEQUENTIAL;
-  } else if ((separator = index(input, * SEPARATOR_CONCURRENT)) != NULL) {
-    // we found a '&' character
-    return SEPARATOR_CONCURRENT;
-  } else if ((separator = index(input, * SEPARATOR_PIPE)) != NULL) {
-    // we found a '|' character
-    return SEPARATOR_PIPE;
-  }
-
-  return separator;
-}
-
-/**
  * Processes a command line entry, extracts jobs and commands, and then saves
  * these commands into an array containing Command structs.
  * @param {char *}     input
@@ -152,7 +132,7 @@ void handle_command_line(
   int pipe,
   Command ** commands
 ) {
-  printf("handle_command_line | [%s]\n", input);
+  //printf("handle_command_line | [%s]\n", input);
   int is_background = 0;
   int is_pipe       = 0;
   char * separator  = NULL;
@@ -189,14 +169,16 @@ void handle_command_line(
 /**
  * Driver program.
  * @return {int}
- */
+
 int main(void) {
   //char sample[] = "date -foo -bar & who & ps -ef | grep foo; ls -l -t -a; who & cat < junk; cat some.file > /tmp/foo & whoami";
   //char sample[] = "date & ps -ef | grep foo; ls -l -t; cat foo.txt > /tmp/foo; cat < junk";
   //char sample[] = "cat | cat | cat | cat > junk & cat | cat | cat | cat | grep line";
-  char sample[] = "ps | sort & sleep 10 & date; cat foo.txt > /tmp/foo";
+  //char sample[] = "ps | sort & sleep 10 & date; cat foo.txt > /tmp/foo";
+  char sample[] = "ls -l > junk ; cat < junk ; /bin/ls -lt /dev/tty* | grep tty | sort | head > junk2 & sleep 10 ; cat < junk2";
   Command * commands[MAX_COMMANDS];
 
   printf("************************************************\n");
   handle_command_line(sample, 0, 0, commands);
 }
+ */
---
+++ command.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SEPARATOR_CONCURRENT "&"
+#define SEPARATOR_INPUT      "<"
+#define SEPARATOR_OUTPUT     ">"
+#define SEPARATOR_PIPE       "|"
+#define SEPARATOR_SEQUENTIAL ";"
+#define MAX_COMMANDS  100
+#define MAX_ARGUMENTS 1000
+
+typedef struct CommandStruct {
+  char * name;
+  char * stdin;
+  char * stdout;
+  char * argv[MAX_ARGUMENTS];
+  int    argc;
+  int    background;
+  int    pipe;
+} Command;
+
+int    get_arguments(char * input, Command * command);
+char * get_separator(char * input);
+void   print_command(Command * command);
+void   make_command(
+  char * input,
+  int background,
+  int pipe,
+  Command * command
+);
+void handle_command_line(
+  char * input,
+  int background,
+  int pipe,
+  Command ** commands
+);
