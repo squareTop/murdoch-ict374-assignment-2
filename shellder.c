78711dff4f369d8bdaef364dd04cb4681dabd33a
Darius Baker <funkage@gmail.com>
2019-11-01 21:49:55 +0800

    refactor(shellder): add test and libraries

--- shellder.c
+++ shellder.c
@@ -1,19 +1,24 @@
 #include "shellder.h"
 
 /**
+ * Run tests.
+ */
+void test() {
+  //char sample[] = "date -foo -bar & who & ps -ef | grep foo; ls -l -t -a; who & cat < junk; cat some.file > /tmp/foo & whoami";
+  //char sample[] = "date & ps -ef | grep foo; ls -l -t; cat foo.txt > /tmp/foo; cat < junk";
+  //char sample[] = "cat | cat | cat | cat > junk & cat | cat | cat | cat | grep line";
+  //char sample[] = "ps | sort & sleep 10 & date; cat foo.txt > /tmp/foo";
+  char sample[] = "ls -l > junk ; cat < junk ; /bin/ls -lt /dev/tty* | grep tty | sort | head > junk2 & sleep 10 ; cat < junk2";
+  printf("************************************************\n");
+  handle_command_line(sample, 0, 0, commands);
+}
+
+/**
  * Driver program.
  * @param  {int}    argc
  * @param  {char *} argv[]
  * @return {int}
  */
 int main(int argc, char * argv[]) {
-  //char sample[] = "date -foo -bar & who & ps -ef | grep foo; ls -l -t -a; who & cat < junk; cat some.file > /tmp/foo & whoami";
-  //char sample[] = "date & ps -ef | grep foo; ls -l -t; cat foo.txt > /tmp/foo; cat < junk";
-  //char sample[] = "cat | cat | cat | cat > junk & cat | cat | cat | cat | grep line";
-  //char sample[] = "ps | sort & sleep 10 & date; cat foo.txt > /tmp/foo";
-  char sample[] = "ls -l > junk ; cat < junk ; /bin/ls -lt /dev/tty* | grep tty | sort | head > junk2 & sleep 10 ; cat < junk2";
-  Command * commands[MAX_COMMANDS];
-
-  printf("************************************************\n");
-  handle_command_line(sample, 0, 0, commands);
+  exit(0);
 }
--- shellder.h
+++ shellder.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "command.h"
+
+/**
+ * Global variables
+ */
+Command * commands[MAX_COMMANDS];
+
+/**
+ * Methods
+ */
+void test();
