b25b850df50647a3233ed1b0323d51d0fb855dbb
Andrew Loone <andrewloone83@gmail.com>
2019-11-18 19:51:17 +0800

    Remove more comments

--- command.c
+++ command.c
@@ -17,19 +17,12 @@ int get_arguments(char * input, Command * command) {
   token = strtok(input, white_space);
 
   while (token != NULL) {
-    /**
-     * Searches for all pathnames matching pattern.
-     * Satisfies:
-     * - Issue #9
-     * - Requirement #6
-     * - Marking guide #5
-     */
+
     if (index(token, WILDCARD_ALL) != NULL || index(token, WILDCARD_EXACT) != NULL) {
       glob_t result;
 
       glob(token, 0, NULL, &result);
 
-      // add expanded patterns to argv
       for (int i = 0; i < result.gl_pathc; i++) {
         command->argv[argument_count] = strdup(result.gl_pathv[i]);
         argument_count++;
@@ -37,10 +30,8 @@ int get_arguments(char * input, Command * command) {
 
       globfree(&result);
     } else {
-      // duplicate string and add to arguments array
       command->argv[argument_count] = strdup(token);
 
-      // increment count
       argument_count++;
     }
 
@@ -52,18 +43,17 @@ int get_arguments(char * input, Command * command) {
 }
 
 
-// Searches for a separator (;&|) and returns the character if found.
+// Searches for a separator and returns the character if found.
 char * get_separator(char * input) {
   char * separator = NULL;
 
   if ((separator = index(input, * SEPARATOR_SEQUENTIAL)) != NULL) {
-    // we found a ';' character
     separator = SEPARATOR_SEQUENTIAL;
-  } else if ((separator = index(input, * SEPARATOR_CONCURRENT)) != NULL) {
-    // we found a '&' character
+  }
+  else if ((separator = index(input, * SEPARATOR_CONCURRENT)) != NULL) {
     return SEPARATOR_CONCURRENT;
-  } else if ((separator = index(input, * SEPARATOR_PIPE)) != NULL) {
-    // we found a '|' character
+  }
+  else if ((separator = index(input, * SEPARATOR_PIPE)) != NULL) {
     return SEPARATOR_PIPE;
   }
 
@@ -79,36 +69,23 @@ Command * make_command(char * input, int background, int pipe) {
   Command * command       = calloc(1, sizeof(Command));
 
   if (stdout_result != NULL) {
-    // output redirection found
     token = strtok(input, SEPARATOR_OUTPUT);
-
-    // get output destination
     token = strtok(NULL, " ");
-
-    // save the redirection; 1 for standard input (fd[1])
     command->redirection = 1;
     command->stdout = strdup(token);
-  } else if (stdin_result != NULL) {
-    // input redirection found
+  }
+  else if (stdin_result != NULL) {
     token = strtok(input, SEPARATOR_INPUT);
-
-    // get input destination
     token = strtok(NULL, " ");
-
-    // save the redirection; 0 for standard input (fd[0])
     command->redirection = 0;
     command->stdin = strdup(token);
   }
 
-  // save other attributes
   command->argc       = get_arguments(input, command);
   command->name       = command->argv[0];
   command->background = background;
   command->pipe       = pipe;
 
-  //print_command(command);
-
-  // only return the struct if there're valid arguments
   if (command->argc > 0) {
     return command;
   } else {
@@ -126,13 +103,11 @@ void handle_command_line(char * input, int background, int pipe, int iteration, 
   char * token      = NULL;
   static int command_count;
 
-  // reset command count if we're running this method as a fresh run
   if (iteration == 0) {
     command_count = 0;
   }
 
   if ((separator = get_separator(input)) != NULL) {
-    // separators found
     token = strtok(input, separator);
     token = strtok(NULL, "");
 
@@ -144,10 +119,9 @@ void handle_command_line(char * input, int background, int pipe, int iteration, 
       is_pipe = command_count + 1;
     }
 
-    // process the input again to see if anymore separators are present
     handle_command_line(input, is_background, is_pipe, 1, commands);
-  } else {
-    // no special characters found
+  }
+  else {
     commands[command_count] = make_command(input, background, pipe);
     command_count++;
   }
